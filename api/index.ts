26b9e6708d4704ec6db8b1033b9a2cf10a95de77
adamaho <aho.adam01@gmail.com>
2023-09-01 00:09:09 -0400

    pub sub?

--- api/index.ts
+++ api/index.ts
@@ -5,13 +5,38 @@ export const config = {
   runtime: "edge",
 };
 
+const clients: Map<string, ReadableStreamDefaultController> = new Map();
 let count = 0;
 
 const app = new Hono().basePath("/api");
 
-app.get("/", (c) => {
+app.get("/subscribe", (c) => {
+  const clientId = `client-${clients.size}`;
+  const body = new ReadableStream({
+    start(controller) {
+      clients.set(clientId, controller);
+      controller.enqueue(new TextEncoder().encode("Client connected...\n"));
+    },
+    cancel() {
+      clients.delete(clientId);
+    },
+  });
+
+  return new Response(body, {
+    headers: {
+      "content-type": "text/plain",
+      "x-content-type-options": "nosniff",
+    },
+  });
+});
+
+app.get("/publish", () => {
   count += 1;
-  return c.json({ count });
+  for (const [key, controller] of clients) {
+    controller.enqueue(new TextEncoder().encode(`count: ${count}`));
+  }
+
+  return new Response("published");
 });
 
 export default handle(app);
